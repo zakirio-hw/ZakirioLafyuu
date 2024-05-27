ba2d15538f22813892c617979598b1ab524f269c
Scklütz <138639029+zakirio-hw@users.noreply.github.com>
2024-05-27 17:38:59 +0700

    Log In

---
+++ app/Exports/_Android_home__temp_/home__temp__activity.java
@@ -0,0 +1,148 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		home__temp_
+	 *	@date 		Monday 27th of May 2024 09:34:03 AM
+	 *	@title 		Screen
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class home__temp__activity extends Activity {
+
+	
+	private View _bg__home__temp_;
+	private ImageView promotion_image;
+	private TextView title;
+	private TextView category_ek1;
+	private TextView more_category_link;
+	private TextView see_more_link;
+	private TextView flash_sale_ek1;
+	private ImageView image_product;
+	private TextView fs___nike_air_max_270_react___;
+	private TextView __299_43;
+	private TextView __534_33;
+	private TextView _24__off;
+	private ImageView image_product_ek1;
+	private TextView fs___quilted_maxi_cros___;
+	private ImageView image_product_ek2;
+	private TextView fs___nike_air_max_270_react____ek1;
+	private ImageView image_51;
+	private TextView title_ek1;
+	private TextView title_ek2;
+	private ImageView image_product_ek3;
+	private TextView nike_air_max_270_react_eng;
+	private ImageView s01tar;
+	private ImageView s02tar;
+	private ImageView s03tar;
+	private ImageView s04tar;
+	private ImageView s05tar;
+	private TextView __299_43_ek1;
+	private TextView __534_33_ek1;
+	private TextView _24__off_ek1;
+	private ImageView image_product_ek4;
+	private TextView nike_air_max_270_react_eng_ek1;
+	private ImageView s01tar_ek1;
+	private ImageView s02tar_ek1;
+	private ImageView s03tar_ek1;
+	private ImageView s04tar_ek1;
+	private ImageView s05tar_ek1;
+	private ImageView image_product_ek5;
+	private TextView nike_air_max_270_react_eng_ek2;
+	private ImageView s01tar_ek2;
+	private ImageView s02tar_ek2;
+	private ImageView s03tar_ek2;
+	private ImageView s04tar_ek2;
+	private ImageView s05tar_ek2;
+	private View rectangle_405;
+	private View line_39;
+	private ImageView vector__stroke_;
+	private ImageView vector__stroke__ek1;
+	private TextView search_product;
+	private ImageView vector__stroke__ek2;
+	private ImageView vector__stroke__ek3;
+	private ImageView vector;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home__temp_);
+
+		
+		_bg__home__temp_ = (View) findViewById(R.id._bg__home__temp_);
+		promotion_image = (ImageView) findViewById(R.id.promotion_image);
+		title = (TextView) findViewById(R.id.title);
+		category_ek1 = (TextView) findViewById(R.id.category_ek1);
+		more_category_link = (TextView) findViewById(R.id.more_category_link);
+		see_more_link = (TextView) findViewById(R.id.see_more_link);
+		flash_sale_ek1 = (TextView) findViewById(R.id.flash_sale_ek1);
+		image_product = (ImageView) findViewById(R.id.image_product);
+		fs___nike_air_max_270_react___ = (TextView) findViewById(R.id.fs___nike_air_max_270_react___);
+		__299_43 = (TextView) findViewById(R.id.__299_43);
+		__534_33 = (TextView) findViewById(R.id.__534_33);
+		_24__off = (TextView) findViewById(R.id._24__off);
+		image_product_ek1 = (ImageView) findViewById(R.id.image_product_ek1);
+		fs___quilted_maxi_cros___ = (TextView) findViewById(R.id.fs___quilted_maxi_cros___);
+		image_product_ek2 = (ImageView) findViewById(R.id.image_product_ek2);
+		fs___nike_air_max_270_react____ek1 = (TextView) findViewById(R.id.fs___nike_air_max_270_react____ek1);
+		image_51 = (ImageView) findViewById(R.id.image_51);
+		title_ek1 = (TextView) findViewById(R.id.title_ek1);
+		title_ek2 = (TextView) findViewById(R.id.title_ek2);
+		image_product_ek3 = (ImageView) findViewById(R.id.image_product_ek3);
+		nike_air_max_270_react_eng = (TextView) findViewById(R.id.nike_air_max_270_react_eng);
+		s01tar = (ImageView) findViewById(R.id.s01tar);
+		s02tar = (ImageView) findViewById(R.id.s02tar);
+		s03tar = (ImageView) findViewById(R.id.s03tar);
+		s04tar = (ImageView) findViewById(R.id.s04tar);
+		s05tar = (ImageView) findViewById(R.id.s05tar);
+		__299_43_ek1 = (TextView) findViewById(R.id.__299_43_ek1);
+		__534_33_ek1 = (TextView) findViewById(R.id.__534_33_ek1);
+		_24__off_ek1 = (TextView) findViewById(R.id._24__off_ek1);
+		image_product_ek4 = (ImageView) findViewById(R.id.image_product_ek4);
+		nike_air_max_270_react_eng_ek1 = (TextView) findViewById(R.id.nike_air_max_270_react_eng_ek1);
+		s01tar_ek1 = (ImageView) findViewById(R.id.s01tar_ek1);
+		s02tar_ek1 = (ImageView) findViewById(R.id.s02tar_ek1);
+		s03tar_ek1 = (ImageView) findViewById(R.id.s03tar_ek1);
+		s04tar_ek1 = (ImageView) findViewById(R.id.s04tar_ek1);
+		s05tar_ek1 = (ImageView) findViewById(R.id.s05tar_ek1);
+		image_product_ek5 = (ImageView) findViewById(R.id.image_product_ek5);
+		nike_air_max_270_react_eng_ek2 = (TextView) findViewById(R.id.nike_air_max_270_react_eng_ek2);
+		s01tar_ek2 = (ImageView) findViewById(R.id.s01tar_ek2);
+		s02tar_ek2 = (ImageView) findViewById(R.id.s02tar_ek2);
+		s03tar_ek2 = (ImageView) findViewById(R.id.s03tar_ek2);
+		s04tar_ek2 = (ImageView) findViewById(R.id.s04tar_ek2);
+		s05tar_ek2 = (ImageView) findViewById(R.id.s05tar_ek2);
+		rectangle_405 = (View) findViewById(R.id.rectangle_405);
+		line_39 = (View) findViewById(R.id.line_39);
+		vector__stroke_ = (ImageView) findViewById(R.id.vector__stroke_);
+		vector__stroke__ek1 = (ImageView) findViewById(R.id.vector__stroke__ek1);
+		search_product = (TextView) findViewById(R.id.search_product);
+		vector__stroke__ek2 = (ImageView) findViewById(R.id.vector__stroke__ek2);
+		vector__stroke__ek3 = (ImageView) findViewById(R.id.vector__stroke__ek3);
+		vector = (ImageView) findViewById(R.id.vector);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/main/java/pnj/ui/ti/zakirio_hugoraazaq_wasis/ui/explore/ExploreViewModel.java
@@ -0,0 +1,19 @@
+package pnj.ui.ti.zakirio_hugoraazaq_wasis.ui.explore;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ExploreViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ExploreViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
